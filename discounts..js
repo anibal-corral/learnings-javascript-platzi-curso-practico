af212425fca4af87b7538283c951e85d2de57e8c
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-05-26 11:12:18 -0400

    Challenge 2 complete

--- discounts..js
+++ discounts..js
@@ -3,11 +3,29 @@ function calculatePriceWithDiscount(price,discount){
     const priceWithDiscount=(price*percentagePriceWithDiscount)/100;
     return priceWithDiscount;
 }
+const cupons = [
+    {
+        name:'cupon10',
+        discount:10
+    },
+    {
+        name:'cupon25',
+        discount:25
+    },
+    {
+        name:'cupon50',
+        discount:50
+    },
+];
 
 function onClickCalculatePriceWithDiscount(){
     const price = document.getElementById("InputPrice").value;
-    const discount = document.getElementById("InputDiscount").value;
+    const selectedCupon = document.getElementById("SelectDiscount").value;
+    const cupon = cupons.find((cupon)=>cupon.name===selectedCupon);
+    
 
-    const finalPrice= calculatePriceWithDiscount(price,discount);
+
+    const finalPrice= calculatePriceWithDiscount(price,cupon.discount);
     document.getElementById("PrecioWithDiscount").innerText=`Final price is ${finalPrice}`;
 }
+
