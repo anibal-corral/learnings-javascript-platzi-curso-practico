1b83c9e5c9b598fcd6bc0d46e739d8489be77b7f
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-05-26 10:56:42 -0400

    HTML Integrated with Javascript

--- discounts..js
+++ discounts..js
@@ -1,8 +1,13 @@
-const originalPrice =100;
-const discount =15;
-
 function calculatePriceWithDiscount(price,discount){
     const percentagePriceWithDiscount=100-discount;
     const priceWithDiscount=(price*percentagePriceWithDiscount)/100;
     return priceWithDiscount;
+}
+
+function onClickCalculatePriceWithDiscount(){
+    const price = document.getElementById("InputPrice").value;
+    const discount = document.getElementById("InputDiscount").value;
+
+    const finalPrice= calculatePriceWithDiscount(price,discount);
+    document.getElementById("PrecioWithDiscount").innerText=`Final price is ${finalPrice}`;
 }
