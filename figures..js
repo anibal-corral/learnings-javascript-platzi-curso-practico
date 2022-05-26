b75342ae2bfd5bdc9cec0f82684d7992a00f64a4
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-05-26 09:08:49 -0400

    encapsulating functions

--- figures..js
+++ figures..js
@@ -1,35 +1,50 @@
 console.group("Square")
 const squareSide=5;
 console.log(`Sides of the square measure: ${squareSide} cm`);
-const squarePerimeter=(squareSide*4);
-console.log(`Perimeter of square: ${squarePerimeter} cm`);
-const squareArea=(squareSide*squareSide);
-console.log(`Area of square: ${squareArea} cm2`);
+function squarePerimeter(side){
+    return side*4;
+}
+console.log(`Perimeter of square: ${squarePerimeter(squareSide)} cm`);
+function squareArea(side){
+    return side*side;
+}
+console.log(`Area of square: ${squareArea(squareSide)} cm2`);
 console.groupEnd();
 
 console.group("Triangle")
 const triangleSideA=6;
 const triangleSideB=6;
-const triangleSideC=4;
-console.log(`Sides of the Triangle measure: ${triangleSideA} cm, ${triangleSideB} cm, ${triangleSideC} cm`);
+const triangleBase=4;
+console.log(`Sides of the Triangle measure: ${triangleSideA} cm, ${triangleSideB} cm, ${triangleBase} cm`);
 const triangleHeight=5.5;
 console.log(`Height of the Triangle measure: ${triangleHeight} cm`);
-const trianglePerimeter=triangleSideA+triangleSideB+triangleSideC;
-console.log(`Perimeter of triangl ${trianglePerimeter}`);
-const triangleArea=(triangleSideC*triangleHeight)/2;
-console.log(`Area of triangl ${triangleArea}`);
+
+function trianglePerimeter(sideA, sideB, base){
+    return sideA+sideB+base;
+}
+console.log(`Perimeter of triangle ${trianglePerimeter(triangleSideA,triangleSideB,triangleBase)}`);
+function triangleArea(base, height){
+    return (base*height)/2;
+}
+
+console.log(`Area of triangl ${triangleArea(triangleBase,triangleHeight)}`);
 console.groupEnd();
 
 console.group("Circle");
-
 const radiusCircle=4;
 console.log(`Radius of Circle ${radiusCircle}`);
-const diameterCircle=radiusCircle*2;
-console.log(`Diameter of Circle ${diameterCircle}`);
-// const PI= 3.1415;
+function diameterCircle(radius){
+return radius*2;
+}
+    
+console.log(`Diameter of Circle ${diameterCircle(radiusCircle)}`);
 const PI = Math.PI;
 console.log(`PI ${PI}`);
-const perimeterCircle = diameterCircle*PI;
-console.log(`Perimeter of Circle ${perimeterCircle}`);
-const areaCircle=(radiusCircle*radiusCircle)*PI;
-console.log(`Area of Circle ${areaCircle}`);
+function perimeterCircle(radius){
+    return diameterCircle(radius)*PI;
+}
+console.log(`Perimeter of Circle ${perimeterCircle(radiusCircle)}`);
+function areaCircle(radius){
+return (radius*radius)*PI;
+}
+console.log(`Area of Circle ${areaCircle(radiusCircle)}`);
